5957b8115547b4c77deb1c8f074f24275c3829ee
Mabel Christina <maybelchristina@gmail.com>
2022-02-12 10:52:26 +0530

    [Mabel]  refactors done and deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/<book-store>'));
+app.use(express.static(__dirname + '/Bookstore_Heroku'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/<book-store>/index.html'));});
+res.sendFile(path.join(__dirname+'/src/index.html'));});
 app.listen(process.env.PORT || 8080);
